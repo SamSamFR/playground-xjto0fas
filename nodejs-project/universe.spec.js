d1e85fec6c263287046ee13b1aeea5713efd9b66
Jérôme Azé <jerome.aze@umontpellier.fr>
2020-04-10 12:09:27 +0200

    les exemples - step 2

--- nodejs-project/universe.spec.js
+++ nodejs-project/universe.spec.js
@@ -1,8 +1,8 @@
-﻿﻿var assert = require('assert');
-var universe = require('./universe.js');
+﻿﻿let assert = require('assert');
+let universe = require('./universe.js');
 
-var reduceUsed = false;
-var superReduce = Array.prototype.reduce;
+let reduceUsed = false;
+let superReduce = Array.prototype.reduce;
 Array.prototype.reduce = function () {
   reduceUsed = true;
   return superReduce.apply(this, arguments);
@@ -10,7 +10,8 @@ Array.prototype.reduce = function () {
 
 it('should sum stars', function () {
   try {
-    assert.equal(6, universe.countAllStars([1, 2, 3]));
+    let galaxies;
+    galaxies = [1, 2, 3]; assert.equal(6, universe.countAllStars(galaxies));
     assert.equal(16, universe.countAllStars([10, 3, 2, 1]));
     assert.equal(42, universe.countAllStars([20, 20, 2]));
 
@@ -30,6 +31,14 @@ it('should sum stars', function () {
     throw error;
   }
 });
+
+function countAllStarsAnswer(galaxies){
+  let totalStars = 0;
+  galaxies.forEach(function(stars) {
+    totalStars += stars; 
+  }, this);
+  return totalStars;
+}
 
 function printMessage(channel, message) {
   console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
